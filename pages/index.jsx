1c50795cf6510a0e4e1ca4b296521991409798e0
Richard Kwon <rkminjae@gmail.com>
2020-08-08 01:01:25 +1200

    Add getStaticProps to fetch firebase projects data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,37 @@
 import Head from 'next/head'
 import Navbar from '../components/navbar'
+import Hero from '../components/hero'
 
-const Home = () => {
+import { loadDB } from '../lib/firebase'
+
+export async function getStaticProps() {
+  const firebaseDb = await loadDB()
+  const db = firebaseDb.firestore()
+  const querySnapshot = await db
+    .collection('portfolio')
+    .doc('projects')
+    .get()
+  
+  const projectData = await querySnapshot
+  .data()  
+  .projects
+    
+  const data = projectData.map(project => project)
+
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+const Home = ({ data }) => {
   return (
     <>
       <header>
+        {console.log(data)}
         <Navbar />
+        <Hero />
       </header>
     </>
   )
