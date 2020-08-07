76f508235222d876a40de2c12c660ea14810dda9
Richard Kwon <rkminjae@gmail.com>
2020-08-07 18:41:02 +1200

    Add components to navbar

---
+++ components/navbar/index.jsx
@@ -0,0 +1,13 @@
+import NavItems from './NavItems'
+import Logo from './Logo'
+
+const Navbar = () => {
+    return (
+        <header className="navbar">
+            <Logo />
+            <NavItems />
+        </header>
+    )
+}
+
+export default Navbar
