e1f51f2857c4c09618b787c1003b1f320c9e33cf
Richard Kwon <rkminjae@gmail.com>
2020-08-08 00:58:43 +1200

    Add content to hero component

---
+++ components/hero/index.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+    return (
+        <section className="hero">
+            <h1 className="hero__header">Richard Kwon</h1>
+            <p className="hero__text">I am a Web Developer that loves to create and learn</p>
+        </section>
+    )
+}
+
+export default Hero 
