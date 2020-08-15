7bae95137a445578b755d64de6df2cf2373c63ed
Richard Kwon <rkminjae@gmail.com>
2020-08-15 15:50:26 +1200

    Add getStaticProps to get projects and skills from firebase firestore

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -15,4 +15,33 @@ const Admin = () => {
     )
 }
 
+export async function getStaticProps() {
+    const projects = []
+    const skills = []
+
+    const queryProjects = await projectDatabase
+        .collection("projects")
+        .get()
+
+    const querySkills = await projectDatabase
+        .collection("skills")
+        .get()
+
+    queryProjects.forEach(docs => {
+        projects.push({...docs.data(), id: docs.id})
+    })
+
+    querySkills.forEach(docs => {
+        skills.push({...docs.data(), id: docs.id})
+    })
+
+    return {
+        props: {
+            projects,
+            skills,
+        },
+        unstable_revalidate: 1,
+    }
+}
+
 export default Admin
