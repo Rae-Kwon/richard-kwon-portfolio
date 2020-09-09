2696c835fa223dcc904c818d947dae4a50a2583d
Richard Kwon <rkminjae@gmail.com>
2020-09-10 01:59:09 +1200

    Add Fuego from swr-firestore for client side rendering with firestore

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,51 +1,67 @@
 import Link from 'next/link'
+import 'firebase/firestore'
+import { Fuego, FuegoProvider } from '@nandorojo/swr-firestore'
 import { projectDatabase } from '../lib/firebase'
 
 import ProjectForm from '../components/admin/projectsForm'
 import SkillsForm from '../components/admin/skillsForm'
 import Projects from '../components/admin/projects'
 
-const Admin = ({ projects, skills }) => {
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+}
+
+const fuego = new Fuego(firebaseConfig)
+
+const Admin = () => {
     return (
-        <div>
-            <Link href="/">
-                <a>Go back</a>
-            </Link>
-            <h1>This is the admin page</h1>
-            <ProjectForm />
-            <SkillsForm />
-            <Projects projectsData={projects} />
-        </div>
+        <FuegoProvider fuego={fuego}>
+            <div>
+                <Link href="/">
+                    <a>Go back</a>
+                </Link>
+                <h1>This is the admin page</h1>
+                <ProjectForm />
+                <SkillsForm />
+                <Projects />
+            </div>
+        </FuegoProvider>
     )
 }
 
-export async function getStaticProps() {
-    const projects = []
-    const skills = []
+// Replace with client side rendering
+// export async function getStaticProps() {
+//     const projects = []
+//     const skills = []
 
-    const queryProjects = await projectDatabase
-        .collection("projects")
-        .get()
+//     const queryProjects = await projectDatabase
+//         .collection("projects")
+//         .get()
 
-    const querySkills = await projectDatabase
-        .collection("skills")
-        .get()
+//     const querySkills = await projectDatabase
+//         .collection("skills")
+//         .get()
 
-    queryProjects.forEach(docs => {
-        projects.push({...docs.data(), id: docs.id})
-    })
+//     queryProjects.forEach(docs => {
+//         projects.push({...docs.data(), id: docs.id})
+//     })
 
-    querySkills.forEach(docs => {
-        skills.push({...docs.data(), id: docs.id})
-    })
+//     querySkills.forEach(docs => {
+//         skills.push({...docs.data(), id: docs.id})
+//     })
 
-    return {
-        props: {
-            projects,
-            skills,
-        },
-        unstable_revalidate: 1,
-    }
-}
+//     return {
+//         props: {
+//             projects,
+//             skills,
+//         },
+//         unstable_revalidate: 1,
+//     }
+// }
 
 export default Admin
