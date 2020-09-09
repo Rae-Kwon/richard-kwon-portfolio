15fc448a4ff41961d2b66c276b35d13c9a0c2c84
Richard Kwon <rkminjae@gmail.com>
2020-09-10 00:43:28 +1200

    Add edit functionality to updateProjects component

--- components/admin/updateProject/index.jsx
+++ components/admin/updateProject/index.jsx
@@ -1,10 +1,30 @@
-const UpdateProject = ({ edit, editHandler }) => {
+import { useState } from 'react'
+import { ProjectDatabase, projectDatabase } from '../../../lib/firebase'
+
+
+const UpdateProject = ({ edit, setEdit, editHandler, projectId }) => {
+    const [projectName, setProjectName] = useState("")
+
+    const editNameHandler = (e) => {
+        const { name, value } = e.target
+        setProjectName({[name]: value})
+        console.log(projectName)
+    }
+
+    const submitEditHandler = async (e) => {
+        e.preventDefault()
+        console.log(projectName.name)
+        await projectDatabase.collection('projects').doc(projectId).update({name: projectName.name})
+        setProjectName({name: ""})
+        setEdit(!edit)
+    }
+
     if (edit) {
         return (
             <div>
-                <form>
-                    <label htmlFor="">
-                        <input type="text" />
+                <form onSubmit={submitEditHandler}>
+                    <label htmlFor="edit-project">
+                        <input name="name" type="text" onChange={editNameHandler} />
                         <input type="submit" value="Edit this" />
                     </label>
                 </form>
