b5b9ce650e7594505de0f5e94001997e19f88d0d
Richard Kwon <rkminjae@gmail.com>
2020-09-23 13:21:22 +1200

    Rework updateProject component to allow multiple form inputs when updating to firebase firestore

--- components/admin/updateProject/index.jsx
+++ components/admin/updateProject/index.jsx
@@ -1,21 +1,34 @@
-import { useState } from 'react'
-import { ProjectDatabase, projectDatabase } from '../../../lib/firebase'
+import { useState } from "react"
+import { projectDatabase } from "../../../lib/firebase"
 
-
-const UpdateProject = ({ edit, setEdit, editHandler, projectId }) => {
-    const [projectName, setProjectName] = useState("")
+const UpdateProject = ({ project, edit, setEdit, editHandler, projectId, section }) => {
+    const [projectData, setProjectData] = useState({
+        name: project.name,
+        summary: project.summary,
+        description: project.description
+    })
 
     const editNameHandler = (e) => {
         const { name, value } = e.target
-        setProjectName({[name]: value})
-        console.log(projectName)
+        setProjectData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }))
     }
 
     const submitEditHandler = async (e) => {
         e.preventDefault()
-        console.log(projectName.name)
-        await projectDatabase.collection('projects').doc(projectId).update({name: projectName.name})
-        setProjectName({name: ""})
+        console.log(project.name, project.description, project.summary)
+        await projectDatabase.collection("projects").doc(projectId).update({
+            name: projectData.name,
+            summary: projectData.summary,
+            description: projectData.description
+        })
+        setProjectData({
+            name: "",
+            summary: "",
+            description: ""
+        })
         setEdit(!edit)
     }
 
@@ -23,12 +36,34 @@ const UpdateProject = ({ edit, setEdit, editHandler, projectId }) => {
         return (
             <div>
                 <form onSubmit={submitEditHandler}>
-                    <label htmlFor="edit-project">
-                        <input name="name" type="text" onChange={editNameHandler} />
-                        <input type="submit" value="Edit this" />
+                    <label htmlFor="name">
+                        Project Name:
+                        <input
+                            name="name"
+                            type="text"
+                            onChange={editNameHandler}
+                        />
                     </label>
+
+                    <label htmlFor="summary">
+                        Project Summary:
+                        <input
+                            name="summary"
+                            type="text"
+                            onChange={editNameHandler}
+                        />
+                    </label>
+
+                    <label htmlFor="description">
+                        Project Description:
+                        <input
+                            name="description"
+                            type="text"
+                            onChange={editNameHandler}
+                        />
+                    </label>
+                    <input type="submit" value="Edit this" />
                 </form>
-                <button onClick={editHandler}>Back</button>
             </div>
         )
     } else {
