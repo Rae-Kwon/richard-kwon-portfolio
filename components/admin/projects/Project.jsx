dc4f41a98222a2d8e0696ef477aefc2a9c4e9e8e
Richard Kwon <rkminjae@gmail.com>
2020-09-23 13:18:22 +1200

    Remove unused components

--- components/admin/projects/Project.jsx
+++ components/admin/projects/Project.jsx
@@ -1,29 +1,28 @@
 import { useState } from "react"
 import UpdateProject from "../updateProject"
 
-const ProjectDescription = ({ project, edit, setEdit, editHandler }) => {
-    // const [edit, setEdit] = useState(false)
-
-    // const editHandler = () => {
-    //     setEdit(!edit)
-    // }
-
+const Project = ({ project, edit, setEdit, editHandler }) => {
     return (
         <div>
-            <h3>Project Description:</h3>
+            <h3>{project.name}</h3>
+            <p>{project.id}</p>
             {edit ? (
                 <UpdateProject
+                    project={project}
                     edit={edit}
                     setEdit={setEdit}
                     editHandler={editHandler}
                     projectId={project.id}
-                    section={"description"}
+                    section={"name"}
                 />
             ) : (
-                <p>{project.description}</p>
+                <>
+                    <p>{project.summary}</p>
+                    <p>{project.description}</p>
+                </>
             )}
         </div>
     )
 }
 
-export default ProjectDescription
+export default Project
