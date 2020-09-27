88116a2423bbe74e172137298aa05d9f587be5bf
Richard Kwon <rkminjae@gmail.com>
2020-09-28 12:15:20 +1300

    Allow skill to be deleted

--- components/admin/deleteSkill/index.jsx
+++ components/admin/deleteSkill/index.jsx
@@ -1,8 +1,28 @@
 import { useState } from 'react'
-const DeleteSkill = () => {
+import { projectDatabase } from '../../../lib/firebase'
+
+const DeleteSkill = ({ skill }) => {
+    const [checkDelete, setCheckDelete] = useState(false)
+
+    const checkDeleteHandler = () => {
+        setCheckDelete(!checkDelete)
+    }
+
+    const deleteSkillHandler = async () => {
+        await projectDatabase.collection("skills").doc(skill.id).delete()
+    }
+    
     return (
         <div>
-            
+            {
+                checkDelete ?
+                <div>
+                    <p>Are you sure you want to delete {skill.name}?</p>
+                    <button onClick={deleteSkillHandler}>Yes</button>
+                    <button onClick={checkDeleteHandler}>No</button>
+                </div> :
+                <button onClick={checkDeleteHandler}>Delete</button>
+            }
         </div>
     )
 }
