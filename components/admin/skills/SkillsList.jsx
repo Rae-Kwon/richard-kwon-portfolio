3ce8796e9b9fcaf4ffabe254428c44d502bf759a
Richard Kwon <rkminjae@gmail.com>
2020-09-28 12:16:00 +1300

    Add DeleteSkill component to SkillsList to render button on each skill

--- components/admin/skills/SkillsList.jsx
+++ components/admin/skills/SkillsList.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import DeleteSkill from '../deleteSkill'
 import Skill from './Skill'
 
 const SkillsList = ({ skill }) => {
     return (
         <div>
             <Skill skill={skill} />
+            <DeleteSkill skill={skill} />
         </div>
     )
 }
