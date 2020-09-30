9ec07765c83d839936590e5dfbe9afacd239ac4a
Richard Kwon <rkminjae@gmail.com>
2020-09-30 21:11:07 +1300

    Create _app page for global styles to reset some default CSS

---
+++ pages/_app.jsx
@@ -0,0 +1,9 @@
+import App from 'next/app'
+import '../public/globalStyles.css'
+
+
+const PortfolioApp = ({ Component, pageProps }) => {
+    return <Component {...pageProps} />
+}
+
+export default PortfolioApp
