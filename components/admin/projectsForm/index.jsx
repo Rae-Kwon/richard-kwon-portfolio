91ecea1be7ca57c1b6071f737a74345c57fb3ce5
Richard Kwon <rkminjae@gmail.com>
2020-10-05 20:04:07 +1300

    Add styles to turn ProjectForm component into a modal

--- components/admin/projectsForm/index.jsx
+++ components/admin/projectsForm/index.jsx
@@ -1,8 +1,15 @@
+import { useState, useRef, useEffect } from "react"
+
 import { useFormInput } from "../../../hooks/customHooks"
+import projectsForm from "./styles.module.scss"
+import AddButton from "../addButton"
 
 import { projectDatabase } from "../../../lib/firebase"
 
 const ProjectForm = () => {
+    const [show, setShow] = useState(false)
+    const modal = useRef(null)
+
     const {
         data: name,
         bind: bindName,
@@ -19,8 +26,13 @@ const ProjectForm = () => {
         reset: resetDescription,
     } = useFormInput("")
 
+    const modalHandler = () => {
+        setShow(!show)
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault()
+        setShow(!show)
         await projectDatabase
             .collection("projects")
             .add({ name, summary, description })
@@ -30,42 +42,59 @@ const ProjectForm = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="name">
-                Name:
-                <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    value={name}
-                    {...bindName}
-                />
-            </label>
+        <>
+            {show ? (
+                <div ref={modal} className={projectsForm.container}>
+                    <form className={projectsForm.form} onSubmit={handleSubmit}>
+                        <h2 className={projectsForm.header}>Add a project</h2>
+                        <label htmlFor="name">
+                            Name:
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                value={name}
+                                {...bindName}
+                            />
+                        </label>
 
-            <label htmlFor="summary">
-                Summary:
-                <input
-                    type="text"
-                    name="summary"
-                    id="summary"
-                    value={summary}
-                    {...bindSummary}
-                />
-            </label>
+                        <label htmlFor="summary">
+                            Summary:
+                            <input
+                                type="text"
+                                name="summary"
+                                id="summary"
+                                value={summary}
+                                {...bindSummary}
+                            />
+                        </label>
 
-            <label htmlFor="description">
-                Description:
-                <input
-                    type="text"
-                    name="description"
-                    id="description"
-                    value={description}
-                    {...bindDescription}
-                />
-            </label>
+                        <label htmlFor="description">
+                            Description:
+                            <input
+                                type="text"
+                                name="description"
+                                id="description"
+                                value={description}
+                                {...bindDescription}
+                            />
+                        </label>
 
-            <input type="submit" value="Send to Firebase" />
-        </form>
+                        <input
+                            type="submit"
+                            value="Send to Firebase"
+                        />
+                        <button type="button" onClick={modalHandler}>
+                            Back
+                        </button>
+                    </form>
+                </div>
+            ) : (
+                <AddButton
+                    modalHandler={modalHandler}
+                />
+            )}
+        </>
     )
 }
 
